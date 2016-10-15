031c0b2657168023344c00bf1407ab1ad32069c7
王东 <373065467@qq.com>
2016-10-15 18:47:11 +0800

    1.创建项目

---
+++ src/com/wd/demo/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.wd.demo;
+
+
+/**
+ * User: WangDong
+ * Time: 2016/10/15 17:30
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+    }
+
+}
